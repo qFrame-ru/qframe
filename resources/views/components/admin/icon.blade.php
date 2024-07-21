345d0cbb7ce9c3facb00caa66b14edc98a515516
Ivan Artamonov <artamonov.i@studio2i.ru>
2024-07-21 23:21:52 +0300

    Отрефакторен компонент иконки

--- resources/views/components/admin/icon.blade.php
+++ resources/views/components/admin/icon.blade.php
@@ -1,9 +1,8 @@
 @isset($text)
 	<span class="icon-text">
 		@isset($icon)
-			<span class="icon">
-				<i class="{{ $collection ?? 'fas' }} fa-{{ $icon }}"></i>
-			</span>
+			@php($collection = $collection ?? 'fas')
+			<x-admin.icon :$collection :$icon />
 		@endisset
 		<span
 			@class(['has-text-weight-bold' => $bold ?? false])
